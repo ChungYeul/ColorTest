41b42eeee8268591e4839aa8c3c1fa77e7399c5a
ChungYeul In <sldk86@naver.com>
2014-01-03 16:21:01 +0900

    실습 완료하였습니다.
    실습 완료하였습니다.

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,21 +9,56 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+- (IBAction)onColorSliderChanged:(id)sender;
 @end
 
 @implementation ViewController
 
+- (IBAction)powerOnOff:(id)sender {
+    BOOL isOn = ((UISwitch *)sender).on;
+    
+    self.redSlider.enabled = isOn;
+    self.greenSlider.enabled = isOn;
+    self.blueSlider.enabled = isOn;
+    
+    if (isOn) {
+        // 액티비티 인디케이터 시작
+        [self.activityIndicator startAnimating];
+        
+        // 1초 뒤에 현재 색상으로 변경
+        [self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0f];
+    }
+    else {
+        // OFF 상태면 회색 상태로 변경
+        self.colorView.backgroundColor = [UIColor grayColor];
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    // 슬라이더의 초기값으로 색상 설정
+    [self onColorSliderChanged:nil];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onColorSliderChanged:(id)sender {
+    // 인디케이터가 동작중이면 중지
+    if ([self.activityIndicator isAnimating]) {
+        [self.activityIndicator stopAnimating];
+    }
+    self.colorView.backgroundColor = [UIColor colorWithRed:self.redSlider.value green:self.greenSlider.value blue:self.blueSlider.value alpha:1.0f];
+}
 @end
